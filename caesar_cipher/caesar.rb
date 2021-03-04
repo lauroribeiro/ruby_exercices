a51443a98b933c322c849c024a6696fafbe6982b
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-03-04 20:33:40 -0300

    caesar cipher

---
+++ caesar_cipher/caesar.rb
@@ -0,0 +1,21 @@
+def caesar(string, key)
+  shift = get_shift(key)
+  string.chars.map do |char|
+    chr = char.ord
+    if chr.between?(65, 90) 
+      chr += shift
+      chr += 26 if chr < 65
+      chr -= 26 if chr > 90
+    elsif chr.between?(97, 122)
+      chr += shift
+      chr += 26 if chr < 97
+      chr -= 26 if chr > 122
+    end
+    chr.chr
+  end
+end
+
+def get_shift(key)
+  return (key % 26) - 26 if key < 0
+  key % 26
+end
